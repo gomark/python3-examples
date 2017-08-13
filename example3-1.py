bf9871884102db35aae1c7300b62be98eaccab00
gomark <putti.tang@gmail.com>
2017-08-14 00:43:24 +0700

    first commit

---
+++ example3-1.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3
+
+import sys
+
+print ('Hello, Python')
+
+counter = 100
+print (counter)
+print ('version=')
+print (sys.version_info)
---
+++ example3-2.py
@@ -0,0 +1,31 @@
+import sys
+import fibo
+
+print ("Hello world")
+
+i = 1
+j = 2
+k = i*j
+str1 = 'my name is putti'
+str2 = ' tang'
+
+print(str1 + ' ' + str2)
+sys.stdout.write('.')
+sys.stdout.write('.')
+
+print ('last char=' + str1[-1])
+
+for i in range(2, 5):
+	print ('i=' + str(i))
+
+def fn1(n):
+	print('fn1=' + n)
+
+fn1('Mark')
+
+fibo.fib(10)
+print (fibo.__name__)
+print ('path=')
+print (*sys.path)
+print (sys.version_info)
+print ('now, my version..?')
